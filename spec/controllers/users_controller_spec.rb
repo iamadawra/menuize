925618ee3777c640e31e3d3c8a00bb91c9f98048
saasbook <saasbook@saasbook.(none)>
2013-10-27 22:54:10 -0700

    added user_controller rspec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  # # This should return the minimal set of attributes required to create a valid
+  # # User. As you add validations to User, be sure to
+  # # adjust the attributes here as well.
+  let(:valid_attributes) { { "name" => "MyString" } }
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # UsersController. Be sure to keep this updated too.
+  let(:valid_session) { {} }
+
+  describe "GET index" do
+    it "assigns all users as @users" do
+      user = User.create! valid_atributes
+      get :index, {}, valid_session
+      assigns(:users).should eq([user])
+    end
+  end
+
+  describe "GET show" do
+    it "assigns the requested user as @user" do
+      user = User.create! valid_attributes
+      get :show, {:id => user.to_param}, valid_session
+      assigns(:user).should eq(user)
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns the requested user as @user" do
+      user = User.create! valid_attributes
+      get :edit, {:id => user.to_param}, valid_session
+      assigns(:user).should eq(user)
+    end
+  end
+
+
+
+end
