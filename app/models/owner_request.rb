309647851040f52219e37c0d18e57cca2fe5c756
Aayush Dawra <aayushgopaldawra@gmail.com>
2013-11-10 14:27:07 -0800

    Bootstrapping Owner Requests front end

--- app/models/owner_request.rb
+++ app/models/owner_request.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: owner_requests
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  restaurant_id   :integer
+#  restaurant_name :string(255)
+#  granted         :integer
+#  granted_by      :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class OwnerRequest < ActiveRecord::Base
   attr_accessible :granted, :granted_by, :restaurant_id, :restaurant_name, :user_id
   # :granted - integer either 1 or 0 for true or false
--- spec/models/owner_request_spec.rb
+++ spec/models/owner_request_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: owner_requests
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  restaurant_id   :integer
+#  restaurant_name :string(255)
+#  granted         :integer
+#  granted_by      :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'spec_helper'
 
 describe OwnerRequest do
