02ffddb6ad0c07a8a64649258c03f454b71b2e8e
saasbook <saasbook@saasbook.(none)>
2013-11-10 15:45:50 -0800

    Add restaurant feature passes

--- features/step_definitions/restaurant_steps.rb
+++ features/step_definitions/restaurant_steps.rb
@@ -1,18 +1,11 @@
-Given /^the current user is "(.*?)" with password "(.*?)"/ do |user, pass|
-  visit '/login/'
-  fill_in("username", :with => user)
-  fill_in("password", :with => pass)
-  click_button("Submit")
-end
-
 Then /^the restaurant "(.*)" should be editable/ do |restaurant|
   r = Restaurant.find_by_name(restaurant)
   r.status.should eq("Collaborative")
 end
 
-Then /^the restaurant "(.*)" should not be editable/ do |restaurant|
+Then /^the restaurant "(.*)" should be awaiting approval/ do |restaurant|
   r = Restaurant.find_by_name(restaurant)
-  r.status.should eq("Exclusive")
+  r.status.should eq("Pending Approval")
 end
 
 Then /^I should reach the show page for (.*)/ do |restaurant|
