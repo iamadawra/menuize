e8bcf2ff3a2cda663ca5d93ab25334c44effc974
Aayush Dawra <aayushgopaldawra@gmail.com>
2013-11-09 23:35:38 -0800

    Updated UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@menuize.herokuapp.com"
+  from: "no-reply@menuize.herokuapp.com"
 
   def registration_confirmation(user)
     @user = user
-    mail(:to => user.email, :subject => "Welcome to Menuize!")
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "Welcome to Menuize!")
   end
 end
 
