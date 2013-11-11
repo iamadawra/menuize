98489c5fbaf76e282c450209b2bd0c67ef936f97
saasbook <saasbook@saasbook.(none)>
2013-11-10 17:25:48 -0800

    Cannot delete while pending even if you are the owner

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,7 +31,6 @@ class Ability
     alias_action :edit, :update, :to => :eu
     can :eu, Restaurant, :status => "Collaborative"
     can :eu, Restaurant, :owned_by => user.id
-    can :destroy, Restaurant, :status => "Pending Approval", :owned_by => user.id
     can :destroy, Restaurant, :status => "Exclusive", :owned_by => user.id
   end
 end
