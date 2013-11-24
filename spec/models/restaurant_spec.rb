ab4babdd64a7ab9613a679e48b988a5370c64304
ziranshang <zshang2@berkeley.edu>
2013-11-24 00:04:36 -0800

    rspec tests for search

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -22,3 +22,45 @@
 
 require 'spec_helper'
 
+describe Restaurant do
+  before(:each) do
+    @test1 = Restaurant.create!("name" => "Subway", "zip" => "94720")
+    @test2 = Restaurant.create!("name" => "Thai Basil", "zip" => "94720")
+    @test3 = Restaurant.create!("name" => "Oscar's Hot Dogs", "zip" => "94709")
+  end
+
+  describe "search by name" do
+    it "should return all restaurants matching the name" do
+      #test_restaurant = Restaurant.create("name" => "Oscar's Hot Dogs", "zip" => "94709")
+      test_by_name = Restaurant.search("Hot Dogs", nil)
+      test_by_name.should have(1).items
+      test_by_name.should include(@test3)
+    end
+    it "should return all restaurants if no name specified" do
+      test_no_name = Restaurant.search(nil, nil)
+      test_no_name.should have(3).items
+      test_no_name.should include(@test1, @test2, @test3)
+    end
+    it "should return no restaurants if none match" do
+      test_none = Restaurant.search("McDonalds", nil)
+      test_none.should have(0).items
+    end
+  end
+
+  describe "search by zip" do
+    it "should return all restaurants matching name and zip" do
+      test_by_zip = Restaurant.search("Subway", "94720")
+      test_by_zip.should have(1).items
+      test_by_zip.should include(@test1)
+    end
+    it "should return all restaurants matching zip if no name" do
+      test_no_name = Restaurant.search(nil, "94709")
+      test_no_name.should have(1).items
+      test_no_name.should include(@test3)
+    end
+    it "should return no restaurants if none match" do
+      test_none = Restaurant.search("Subway", "94709")
+      test_none.should have(0).items
+    end
+  end
+end
