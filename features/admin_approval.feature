c54621de8d1ee17d76ea3c7aacff87f0a6c501f9
saasbook <saasbook@saasbook.(none)>
2013-12-07 00:40:55 -0800

    admin approval feature test passes - Saveen

--- features/admin_approval.feature
+++ features/admin_approval.feature
@@ -6,21 +6,33 @@ So that I can approve or deny ownership requests
 
 Background:
   Given the following restaurants exist:
-  | name | zip   | address | menu  | owned_by | status |
-  | Thai Basil | 94704 | 2 abc St, Berkeley  |  thai combo,fried rice | 2 | Exclusive |
-  | Racha | 94704 | 3 abc St, Berkeley | soup | 1 | Pending Approval |
-  | Taco Bell | 9470 | 4 abc St, Berkeley | burrito | | Collaborative |
+  | id | name | zip   | address | menu  | owned_by | status |
+  | 1| Pasta Benne | 94704 | 1 abc St, Berkeley  | pizza,pasta | 1 | Exclusive |
+  | 2| Thai Basil | 94704 | 2 abc St, Berkeley  |  thai combo,fried rice | 2 | Exclusive |
+  | 3| Racha | 94704 | 3 abc St, Berkeley | soup | 1 | Pending Approval |
+  | 4| Taco Bell | 9470 | 4 abc St, Berkeley | burrito | | Collaborative |
+
+  And the following users exist:
+  | email | password | id |
+  | johnsnow@gmail.com | john123 | 1 |
+  | sampeter@yahoo.com | sam123  | 2 |
+  | george@gmail.com | george123 | 3 |
+
+  And "johnsnow@gmail.com" with password "john123" has requested ownership of "Taco Bell"
+  And I am logged in as admin
+  And I visit the admin page for pending approvals
   
-  And I am on the admin page
-  
-Scenario: Approve a user's ownership request
-When a user requests ownership of "Taco Bell"
-And the request is legitimate
-And I press "Approve Request"
-Then I should see "Request was successfully approved"
+Scenario: Admin page shows user ownership requests
+Then I should see "Taco Bell"
+
+Scenario: Approve an owner's request
+Given I follow "Approve" for "Taco Bell"
+Then I should not see "Taco Bell"
+And "johnsnow@gmail.com" with password "john123" should be able to edit "Taco Bell"
+And "sampeter@yahoo.com" with password "sam123" should not be able to edit "Taco Bell"
   
 Scenario: Reject a user's ownership request
-When a user requests ownership of "Taco Bell"
-And the request is not legitimate
-And I press "Deny Request"
-Then I should see "Request was successfully denied"
+Given I follow "Deny" for "Taco Bell"
+Then I should not see "Taco Bell"
+And "johnsnow@gmail.com" with password "john123" should be able to edit "Taco Bell"
+And "sampeter@yahoo.com" with password "sam123" should be able to edit "Taco Bell"
---
+++ features/step_definitions/admin_steps.rb
@@ -0,0 +1,69 @@
+def login(username, password)
+  visit "/login"
+  fill_in("email", :with => username)
+  fill_in("password", :with => password)
+  click_button("Log in")
+end
+
+Given /^"(.*)" with password "(.*)" should be able to edit "(.*)"/ do |username, password, rest|
+  login(username, password)
+  visit "restaurants/" + Restaurant.find_by_name(rest).id.to_s
+  Then "I should see \"Edit Restaurant\""
+end
+
+Given /^"(.*)" with password "(.*)" should not be able to edit "(.*)"/ do |username, password, rest|
+  login(username, password)
+  visit "restaurants/" + Restaurant.find_by_name(rest).id.to_s
+  Then "I should not see \"Edit Restaurant\""
+end
+
+Given /^I follow "(.*)" for "(.*)"/ do |action, restaurant|
+  id = Restaurant.find_by_name(restaurant).id
+  all("tr").each do |tr|
+    if tr.has_content?(restaurant)
+      tr.find_link(action).click
+      break
+    end
+  end
+end
+
+Given /^I visit the admin page for pending approvals/ do
+  visit "/admin/restaurants?order=id_desc&page=1&scope=pending_approval"
+end
+
+Given /^"(.*)" with password "(.*)" has requested ownership of "(.*)"/ do |user, password, restaurant|
+  login(user, password)
+  restaurant_id = Restaurant.find_by_name(restaurant).id
+  visit("/restaurants/" + restaurant_id.to_s + "/edit")
+  Given "I select \"Exclusive\" from \"restaurant[status]\""
+  And "I press \"Update Restaurant\""
+end
+
+Given /^I am logged in as admin$/ do
+  create_admin_user_and_logout_if_needed
+  user = AdminUser.find_by_email "admin@example.com"
+  visit "/admin/login"
+  fill_in "Email", :with => "admin@example.com"
+  fill_in "Password", :with => "password"
+  click_button "Login"
+  Then "I should see \"Signed in successfully.\""
+end
+
+def create_admin_user_and_logout_if_needed
+  step 'an admin user "admin@example.com" exists'
+
+  if page.all(:css, "a", :text => "Logout").size > 0
+    click_link "Logout"
+  end
+end
+
+Given /^an admin user "([^"]*)" exists$/ do |admin_email|
+  user = AdminUser.find_or_create_by_email :email => admin_email,
+                                           :password => "password",
+                                           :password_confirmation => "password"
+
+  unless user.persisted?
+    puts "Coult not create an admin user #{admin_email}: #{user.errors.full_messages}"
+    raise "Could not create an admin user"
+  end
+end
